0ba5edebf026624f7444502365940bb10ed120d1
unknown <zaiidan06@gmail.comclsclear>
2024-01-01 00:21:39 +0700

    test

---
+++ src/i18n/en-US/index.ts
@@ -0,0 +1,36 @@
+// This is just an example,
+// so you can safely delete all default props below
+
+export default {
+  notes: 'Notes',
+  title: 'ARCHIPELAGO',
+  failed: 'Action failed',
+  success: 'Action was successful',
+  addHotel: 'Add Hotel',
+  addNote: 'Add Note',
+  editNote: 'Edit Note',
+  deleteNote: 'Delete Note',
+  HotelName: 'Hotel Name',
+  DistrictName: 'District Name',
+  HotelNotEmpty: 'Notes cannot be empty',
+  DistrictNotEmpty: 'Created By cannot be empty',
+  confirmDeleteNote: 'Are you sure you want to delete the not',
+  noLabel: 'No',
+  createdStatusLabel: 'Notes Status',
+  actionLabel: 'Action',
+  noColumn: 'No',
+  notesColumn: 'Notes',
+  createdAtColumn: 'Created At',
+  createdByColumn: 'Created By',
+  statusColumn: 'Notes Status',
+  actionColumn: 'Action',
+  headline: 'Find a Hotel Near You!',
+  columns : {
+    no: 'No',
+    HotelName: 'Hotel Name',
+    DistrictName: 'District Name',
+    created_at: 'Created At',
+    status: 'Hotel Status',
+    action: 'Action'
+  }
+};
---
+++ src/i18n/id/index.ts
@@ -0,0 +1,35 @@
+// This is just an example,
+// so you can safely delete all default props below
+
+export default {
+  title: 'ARCHIPELAGO',
+  failed: 'Gagal',
+  success: 'Berhasil',
+  addHotel: 'Tambah Hotel',
+  addNote: 'Tambah Catatan',
+  editNote: 'Edit Catatan',
+  deleteNote: 'Hapus Catatan',
+  HotelName: 'Nama Hotel',
+  DistrictName: 'Daerah',
+  HotelNotEmpty: 'Catatan tidak boleh kosong',
+  DistrictNotEmpty: 'Dibuat Oleh tidak boleh kosong',
+  confirmDeleteNote: 'Apakah Anda yakin ingin menghapus catatan',
+  noLabel: 'Nomor',
+  createdStatusLabel: 'Status Catatan',
+  actionLabel: 'Aksi',
+  noColumn: 'Nomor',
+  notesColumn: 'Catatan',
+  createdAtColumn: 'Dibuat Pada',
+  createdByColumn: 'Dibuat Oleh',
+  statusColumn: 'Status Catatan',
+  actionColumn: 'Aksi',
+  headline: 'Temukan Hotel Terdekat!',
+  columns : {
+    no: 'Nomor',
+    HotelName: 'Nama Hotel',
+    DistrictName: 'Nama Daerah',
+    created_at: 'Dibuat pada',
+    status: 'Status Hotel',
+    action: 'Aksi'
+  }
+};
